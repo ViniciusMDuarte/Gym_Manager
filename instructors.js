e7ca529167fe873f49fd84424bd7e01e958a78b5
ViniciusMDuarte <viniduarte222@gmail.com>
2020-08-02 17:50:44 -0300

    create page register instructor

---
+++ instructors.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const data = require('./data.json')
+
+
+// create
+exports.post = function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields !')
+        }
+    }
+
+    let { avatar_url, birth, name, type_class, scholling, services, gender } = req.body
+
+    birth = Date.parse(birth )
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    data.instructors.push( {
+        id,
+        avatar_url,
+        name,
+        gender,
+        birth,
+        type_class,
+        scholling,
+        services,
+        created_at,
+    })
+
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2 ), function(err) {
+        if(err) return res.send('Write fille error')
+
+        return res.redirect('/instructors')
+    } )
+
+    // return res.send(req.body)
+}
+
